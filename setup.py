72e9124b43580bc5d14038dd7e031a8707f41bb0
Kiran Karra <Kiran.Karra@jhuapl.edu>
2019-10-15 10:20:45 -0400

    added Neil to Author list

--- setup.py
+++ setup.py
@@ -5,9 +5,9 @@ import setuptools
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-__author__ = 'Kiran Karra, Chace Ashcraft, Nat Kavaler, Nathan Drenkow, Cash Costello'
+__author__ = 'Kiran Karra, Chace Ashcraft, Nat Kavaler, Neil Fendley, Nathan Drenkow, Cash Costello'
 __email__ = 'kiran.karra@jhuapl.edu,chace.ashcraft@jhuapl.edu,nathaniel.kavaler@jhaupl.edu,' \
-            'nathan.drenkow@jhuapl.edu,cash.costello@jhuapl.edu'
+            'neil.fendley@jhuapl.edu', 'nathan.drenkow@jhuapl.edu,cash.costello@jhuapl.edu'
 __version__ = '0.2.6'
 
 
